303cb3ca0027c0fdbe79fb9d6ed11d2059045180
Shawn Allen <shawn.allen@gsa.gov>
2016-04-23 09:31:35 -0700

    first stab at porting to node, cleaning up

---
+++ index.js
@@ -0,0 +1,198 @@
+var d3 = require('d3');
+var topojson = require('topojson');
+var copy = require('deep-copy');
+
+var projectArcs = require('./lib/projectArcs');
+var objectify = require('./lib/objectify');
+var math = require('./lib/math');
+
+module.exports = function() {
+
+  var iterations = 8;
+  var projection = d3.geo.albers();
+  var properties = function(geom, topology) {
+    return {};
+  };
+  var value = function(d) {
+    return 1;
+  };
+
+  var path = d3.geo.path()
+    .projection(null);
+
+  var cartogram = function(topology, geometries) {
+    console.time('copy');
+    topology = copy(topology);
+    console.timeEnd('copy');
+
+    console.time('project');
+    var projectedArcs = projectArcs(topology, projection);
+    console.timeEnd('project');
+
+    console.time('objectify');
+    var objects = objectify(projectedArcs, {
+      type: "GeometryCollection",
+      geometries: geometries
+    })
+    .geometries.map(function(geom) {
+      return {
+        type: "Feature",
+        id: geom.id,
+        properties: properties.call(null, geom, topology),
+        geometry: geom
+      };
+    });
+    console.timeEnd('objectify');
+
+    var values = objects.map(value);
+    var totalValue = d3.sum(values);
+
+    if (iterations <= 0) {
+      return objects;
+    }
+
+    var i = 0;
+    console.time('iterate');
+    while (i++ < iterations) {
+      console.time('iteration ' + i);
+      var areas = objects.map(path.area);
+      var totalArea = d3.sum(areas);
+      var sizeErrorsTot = 0;
+      var sizeErrorsNum = 0;
+      var meta = objects.map(function(o, j) {
+        // FIXME: why do we have negative areas?
+        var area = Math.abs(areas[j]);
+        var v = +values[j],
+        var desired = totalArea * v / totalValue;
+        var radius = Math.sqrt(area / Math.PI);
+        var mass = Math.sqrt(desired / Math.PI) - radius;
+        sizeError = Math.max(area, desired) / Math.min(area, desired);
+        sizeErrorsTot += sizeError;
+        sizeErrorsNum++;
+        // console.log(o.id, "@", j, "area:", area, "value:", v, "->", desired, radius, mass, sizeError);
+        return {
+          id:         o.id,
+          area:       area,
+          centroid:   path.centroid(o),
+          value:      v,
+          desired:    desired,
+          radius:     radius,
+          mass:       mass,
+          sizeError:  sizeError
+        };
+      });
+
+      var sizeError = sizeErrorsTot / sizeErrorsNum;
+      var forceReductionFactor = 1 / (1 + sizeError);
+
+      // console.log("meta:", meta);
+      // console.log("  total area:", totalArea);
+      // console.log("  force reduction factor:", forceReductionFactor, "mean error:", sizeError);
+
+      var len2 = projectedArcs.length;
+      var i2 = 0;
+
+      while (i2 < len2) {
+        var len1 = projectedArcs[i2].length;
+        var i1 = 0;
+        while (i1 < len1) {
+          // create an array of vectors: [x, y]
+          var delta = [0,0];
+          var len3 = meta.length;
+          var i3 = 0;
+          while (i3 < len3) {
+            var centroid = meta[i3].centroid;
+            var mass = meta[i3].mass;
+            var radius = meta[i3].radius;
+            var rSquared = radius * radius;
+            var dx = projectedArcs[i2][i1][0] - centroid[0];
+            var dy = projectedArcs[i2][i1][1] - centroid[1];
+            var distSquared = dx * dx + dy * dy;
+            var dist = Math.sqrt(distSquared);
+            var Fij = (dist > radius)
+              ? mass * radius / dist
+              : mass *
+                (distSquared / rSquared) *
+                (4 - 3 * dist / radius); // XXX magic numbers!
+            delta[0] += (Fij * math.cosArctan(dy, dx));
+            delta[1] += (Fij * math.sinArctan(dy, dx));
+            i3++;
+          }
+          projectedArcs[i2][i1][0] += delta[0] * forceReductionFactor;
+          projectedArcs[i2][i1][1] += delta[1] * forceReductionFactor;
+          i1++;
+        }
+        i2++;
+      }
+      console.time('iteration ' + i);
+
+      // break if we hit the target size error
+      if (sizeError <= 1) break;
+    }
+    console.timeEnd('iterate');
+
+    return {
+      features: objects,
+      arcs: projectedArcs
+    };
+  };
+
+  // expose the path directly, for convenience
+  cartogram.path = path;
+
+  cartogram.iterations = function(i) {
+    if (arguments.length) {
+      iterations = Number(i) || 0;
+      return cartogram;
+    } else {
+      return iterations;
+    }
+  };
+
+  cartogram.value = function(v) {
+    if (arguments.length) {
+      value = d3.functor(v);
+      return cartogram;
+    } else {
+      return value;
+    }
+  };
+
+  cartogram.projection = function(p) {
+    if (arguments.length) {
+      projection = p;
+      return cartogram;
+    } else {
+      return projection;
+    }
+  };
+
+  cartogram.feature = function(topology, geom) {
+    return {
+      type: "Feature",
+      id: geom.id,
+      properties: properties.call(null, geom, topology),
+      geometry: {
+        type: geom.type,
+        coordinates: topojson.object(topology, geom).coordinates
+      }
+    };
+  };
+
+  cartogram.features = function(topo, geometries) {
+    return geometries.map(function(f) {
+      return cartogram.feature(topo, f);
+    });
+  };
+
+  cartogram.properties = function(props) {
+    if (arguments.length) {
+      properties = d3.functor(props);
+      return cartogram;
+    } else {
+      return properties;
+    }
+  };
+
+  return cartogram;
+};
