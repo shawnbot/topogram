24276864ac97a357a3aa1194c5e52bcb7c6318bc
Shawn Allen <shawn.allen@github.com>
2018-04-13 11:07:58 -0700

    fix up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,27 @@
 import commonjs from "rollup-plugin-commonjs";
 import nodeResolve from "rollup-plugin-node-resolve";
 
-
 export default {
   entry: 'index.js',
-  moduleName: 'd3',
+  moduleName: 'topogram',
   format: 'umd',
-  globals: {
-    'd3-geo': 'd3',
-    'd3-array': 'd3'
-  },
-  dest: 'build/d3-cartogram.js',
+  dest: 'build/topogram.js',
   plugins: [
     nodeResolve({
-        jsnext: true,
-        main: true,
-        browser: true,
-        extensions: [".js", ".jsx"]
-      }),
+      jsnext: true,
+      main: true,
+      browser: true,
+      extensions: ['.js'],
+    }),
     commonjs({
       include: "node_modules/**",
-      exclude: [ "node_modules/d3-geo/", "node_modules/d3-array/", "node_modules/buffer/", "node_modules/events/", "node_modules/util/"]
-
+      exclude: [
+        "node_modules/d3-geo/",
+        "node_modules/d3-array/",
+        "node_modules/buffer/",
+        "node_modules/events/",
+        "node_modules/util/"
+      ]
     })
   ]
 };
